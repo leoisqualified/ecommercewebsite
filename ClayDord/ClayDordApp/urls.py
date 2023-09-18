03267192c7cc7849e54bda0a083c7d8d455f2b1f
andy <eaadenutsi@st.ug.edu.gh>
2023-09-18 21:23:56 +0000

    second commit

--- ClayDord/ClayDordApp/urls.py
+++ ClayDord/ClayDordApp/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('cart/',views.cartPage,name = 'cart'),
 
     #Forget Password paths
-    path('reset_password',auth_views.PasswordResetView.as_view(), name = 'reset_password'),
+    path('reset_password',auth_views.PasswordResetView.as_view(template_name = 'password_reset.html'), name = 'reset_password'),
     path('password_reset_done',auth_views.PasswordResetDoneView.as_view(),name = 'password_reset_done'),
     path('password/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(), name = 'password_reset_confirm'),
     path('password_reset_complete',auth_views.PasswordResetCompleteView.as_view(),name = 'password_reset_complete')
