2a12c8500329fb1a1be969fad3857aae329c5e5f
andy <eaadenutsi@st.ug.edu.gh>
2023-09-19 09:21:43 +0000

    added smtp configuration

--- ClayDord/ClayDord/settings.py
+++ ClayDord/ClayDord/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP_CONF 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'your_username@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
