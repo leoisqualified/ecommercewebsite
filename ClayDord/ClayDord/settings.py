64847423310de0f6d0c97c0915fb03496c06b22d
andy <eaadenutsi@st.ug.edu.gh>
2023-09-30 02:44:31 +0000

    restructured the shop page

--- ClayDord/ClayDord/settings.py
+++ ClayDord/ClayDord/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 
 # Default primary key field type
@@ -133,3 +138,4 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
